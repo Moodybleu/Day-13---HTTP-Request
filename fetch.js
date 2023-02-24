8a75a2507a37d4e96a58b571e6204b955ab5b33b
Moodybleu <meganbenn27@icloud.com>
2023-02-24 14:39:27 -0800

    Completes project

--- fetch.js
+++ fetch.js
@@ -6,6 +6,23 @@ function getUsers(e) {
 
     fetch("users.json")
         .then(function(response) {
-            return response.json
+            return response.json();
         })
-}
+        .then(function(data) {
+            console.log(data)
+            let output = "";
+            data.forEach(function(user) {
+                output +=   
+                `
+                <hr>
+                <ul>
+                    <li>ID: ${user.id}</li>
+                    <li>Name: ${user.name}</li>
+                    <li>Age: ${user.age}</li>
+                    <li>Email: ${user.email}</li>
+                </ul>
+                `;
+            })
+            document.getElementById("users").innerHTML = output;
+        })
+    }
